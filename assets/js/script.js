f98908b9cf6ee289d9f72e3dedcf6c06c167f501
Carol Santibañez <casrdominica@yahoo.es>
2020-12-27 17:50:42 -0300

    cambio6delPortafolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
-$('#enviarCorreo').click(function(){
-    alert("El correo fue enviado correctamente...")
-});
+$(function () {
+    $('body').on('click', 'h4', function(){
+        $(this).css("color","green")
+    });
+    $('[data-toggle="tooltip"]').tooltip()
+
+    $('#enviarCorreo').click(function () {
+        alert("El correo fue enviado correctamente...")
+    });
+    $('.card-title').click(function () {
+        $('.card-text').toggle()
+    });
+})
